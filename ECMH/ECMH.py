b4867626e23d3c175be03edbb4f1e2b3de2bb8b5
Wang Xinyang <112737241+wxy-sudo@users.noreply.github.com>
2023-07-11 21:50:56 +0800

    Update ECMH.py

--- ECMH/ECMH.py
+++ ECMH/ECMH.py
@@ -2,6 +2,7 @@ import hashlib
 import math
 import random
 
+
 def Legendre_symbol(a, p): 
     return pow(n, (p - 1) // 2, p)
 
@@ -33,7 +34,7 @@ def modular_sqrt(a, p):
             e = z
     return x
 
-def elliptic_curve_hash(u):
+def ech(u):
     h = int(hashlib.sha256(str(u).encode()).hexdigest(), 16)
     for i in range(0, p):
         x = (h + i) % p
@@ -43,10 +44,10 @@ def elliptic_curve_hash(u):
             return [x, y]
     return None
 
-def elliptic_curve_hash_set(s):
+def echs(s):
     result = [0, 0]
     for x in s:
-        point = elliptic_curve_hash(x)
+        point = ech(x)
         if point is not None:
             result[0] += point[0]
             result[1] += point[1]
@@ -60,24 +61,30 @@ if __name__ == '__main__':
     n = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
     G = 0x32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7, 0xbc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0
 
-    m1 = 'liujiaming'
-    m2 = 'sdu'
-    h1 = elliptic_curve_hash(m1)
-    h2 = elliptic_curve_hash(m2)
+    m1 = 'wxy'
+    m2 = '10086'
+    h1 = ech(m1)
+    h2 = ech(m2)
 
-    print("Elliptic Curve Message Hash of '{}':\n{}".format(m1, h1))
-    print("Elliptic Curve Message Hash of '{}':\n{}".format(m2, h2))
+    print()
+    print("'{}'hash value:\n{}".format(m1, h1))
+    print("'{}'hash value:\n{}".format(m2, h2))
     s1 = {m1, m2}
-    h3 = elliptic_curve_hash_set(s1)
-    print("Elliptic Curve Message Hash Set of {}: \n{}".format(s1, h3))
+    h3 = echs(s1)
+    
+    print()
+    print("{} set hash value: \n{}".format(s1, h3))
     if (h3[0] == h2[0] + h1[0] and h3[1] == h2[1] + h1[1]):
         print("The sum of ECMH('{}') and ECMH('{}') matches ECMH({})".format(m1, m2, s1))
     else:
         print("The sum of ECMH('{}') and ECMH('{}') does not match ECMH({})".format(m1, m2, s1))
     s3 = s1 - {m1}
-    h3 = elliptic_curve_hash_set(s3)
-    print("Elliptic Curve Message Hash Set of {} after removing '{}':\n{}".format(s1, m1, h3))
+    h3 = echs(s3)
+    print()
+    print("{} set hash value: after removing '{}':\n{}".format(s1, m1, h3))
+    print()
     if (h3 == h2):
         print("ECMH({} - '{}') matches ECMH('{}')".format(s1, m1, m2))
     else:
         print("ECMH({} - '{}') does not match ECMH('{}')".format(s1, m1, m2))
+    print()
